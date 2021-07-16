ace22468a9f60ad7811a9dabb7e7509b63d0c9ba
sgdlhy <1010235334@qq.com>
2021-07-16 21:58:38 +0800

    完成了主页开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,5 +6,23 @@ $(function(){
        // console.log(options.url)
        // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
        options.url='http://api-breakingnews-web.itheima.net'+options.url
+
+       if(options.url.indexOf('/my/')!==-1){
+           options.headers={
+                Authorization:localStorage.getItem('token') || ''
+           }
+       }
+
+      //统一全局complete回调函数
+      options.complete=function(res){
+            //console.log(res)
+            //使用res.responseJSON拿到服务器响应回来的数据
+            if(res.responseJSON.status==1 && res.responseJSON.message =='身份认证失败！'){
+                //强制情况token
+                localStorage.removeItem('token')
+                //强制跳转到登录页面
+                location.href='/login.html'
+            }
+        }
     })
 })
---
+++ assets/js/index.js
@@ -0,0 +1,62 @@
+$(function(){
+    //调用 获取用户信息
+    getUserInfo()
+
+    var layer=layui.layer
+    $('#btnLogout').on('click',function(){
+        layer.confirm('确认退出登录?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            //1.清除本地存储的token
+            localStorage.removeItem('token')
+            //重新跳转到登录页面
+            location.href='/login.html'
+            //关闭询问框
+            layer.close(index);
+          });
+    })
+})
+
+//获取用户基本信息
+function getUserInfo(){
+    $.ajax({
+        url:'/my/userinfo',
+        method:'GET',
+       /*  headers:{
+            Authorization:localStorage.getItem('token') || ''
+        }, */
+        success: function(res){
+           //console.log(res)
+            if(res.status !== 0){
+                return layui.layer.msg('获取信息失败')
+            }
+            renderAvatar(res.data)
+        },
+
+        //无论成功还是失败，最终都会调用complete回调函数
+      /*   complete:function(res){
+            //console.log(res)
+            //使用res.responseJSON拿到服务器响应回来的数据
+            if(res.responseJSON.status==1 && res.responseJSON.message =='身份认证失败！'){
+                //强制情况token
+                localStorage.removeItem('token')
+                //强制跳转到登录页面
+                location.href='/login.html'
+            }
+        } */
+  
+    })
+}
+
+function renderAvatar(user){
+    var name=user.nichname||user.username
+    $('#welcome').html('欢迎&nbsp'+name)
+    if(user.user_pic !== null){
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-avatar').hide()
+    }else{
+        $('.layui-nav-img').hide()
+        var first=name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+
+}
